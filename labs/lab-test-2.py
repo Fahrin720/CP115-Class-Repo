1284ef36acf47b6c2022d1b2dc83e3b726eec843
Fahrin720 <muneerfahrin@gmail.com>
2025-08-11 12:57:45 +0800

    lab test is here

---
+++ labs/lab-test-2.py
@@ -0,0 +1,21 @@
+# Program by Mohamed Fahrin Bin Mohamed Muneer from class C02
+# Make a program to answer the lab question
+
+# Assign Variables
+student_info = "Name: Ahmad Bin Abu\t\t\t\t Matric. No: MS2025123499"
+
+pattern1 = "*\t\t\t*\n"
+pattern2 = "**\t\t**\n"
+pattern3 = "***\t***\n"
+pattern4 = "********\n"
+
+# Aritmetic Process
+marks = 2 * 10
+
+# Output for the whole program
+print(student_info, "\n\n")
+print(pattern1 + pattern2 + pattern3 + pattern4 + pattern3 + pattern2 + pattern1)
+print("\n\nThis is my\n\tsecond\n\t\tassignment\nI want 2x10 marks, which is", marks, "full marks\n")
+
+
+
