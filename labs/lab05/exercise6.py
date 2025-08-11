b2302b30e204ca0981a8915ecb545d1fb2567d7b
Fahrin720 <muneerfahrin@gmail.com>
2025-08-11 12:11:48 +0800

    lab 6

--- labs/lab05/exercise6.py
+++ labs/lab05/exercise6.py
@@ -5,5 +5,5 @@ time_minutes = float(input("Enter the time in minutes: "))
 
 time_hours = time_minutes / 60
 
-print()
+print(time_minutes, "minutes")
 print("The time in hours is:", time_hours, "hours")
